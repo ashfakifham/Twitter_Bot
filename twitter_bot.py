65cd4898c549b3b68369abc12a544bf92a0e86d0
ashfakifham <ashfak.ifham@outlook.com>
2021-01-06 21:26:37 +0530

    Twitter bot that posts status of tweet. this will be used to post for the 100 days of code.

---
+++ twitter_bot.py
@@ -0,0 +1,36 @@
+import tweepy as tp
+import keys as key
+
+# setting authentication for the api
+auth = tp.OAuthHandler(key.consumer_key, key.consumer_secret)
+auth.set_access_token(key.access_token, key.access_token_secret)
+
+# creating api object
+api = tp.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+my_id = "" # for user ID
+
+# to get user entry for days count and tweet to be posted
+# day = input("Enter which day this is")
+# time_spent = input("Enter how long you coded")
+day_counter = []
+day_count=1
+
+
+
+
+statuses =api.user_timeline(my_id, count = 2) #extracting the last  2 status updates
+
+for status in statuses:
+    #print(status.text)
+    my_day, my_num, my_data = str(status.text).split(" ",2) # only splits to a max of 2 spaces
+    day_counter.append(int(my_num))
+
+day_count += max(day_counter)
+status = "Day {} of my #100DaysOfCode Challenge:\nCompleted the twitter bot development and this is its first tweet!".format(day_count)
+
+
+# api.update_status(status)
+#api.update_status("Im a bot and lets hope I work!")
+# user = api.get_user(sreen_name = "") to get a specific user's ID to dm
+# print(user.id) # extracting the id value form the json file
+# api.send_direct_message(user.id,"Since im working, Happy tweeting!") #DM
