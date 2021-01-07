5bbd6a19280ec79270d67db2756b1a03f9235a11
ashfakifham <ashfak.ifham@outlook.com>
2021-01-07 23:17:59 +0530

    added some json manipulation lines to understand working with apis further

--- twitter_bot.py
+++ twitter_bot.py
@@ -1,5 +1,6 @@
 import tweepy as tp
 import keys as key
+import json
 
 # setting authentication for the api
 auth = tp.OAuthHandler(key.consumer_key, key.consumer_secret)
@@ -19,15 +20,18 @@ day_count=1
 
 
 statuses =api.user_timeline(my_id, count = 2) #extracting the last  2 status updates
+tweet = statuses[0]
+data = json.dumps(tweet._json, indent = 2) #creates a json object ( json string)
+data_2 = json.loads(data) # converts a json string to python objects based on json convertion logic
 
-for status in statuses:
-    #print(status.text)
-    my_day, my_num, my_data = str(status.text).split(" ",2) # only splits to a max of 2 spaces
-    day_counter.append(int(my_num))
+#for status in statuses:
+   #print(status.text)
+   # my_day, my_num, my_data = str(status.text).split(" ",2) # only splits to a max of 2 spaces
+   # day_counter.append(int(my_num))
 
-day_count += max(day_counter)
+#day_count += max(day_counter)
 status = "Day {} of my #100DaysOfCode Challenge:\nCompleted the twitter bot development and this is its first tweet!".format(day_count)
-
+print(data_2['text'])
 
 # api.update_status(status)
 #api.update_status("Im a bot and lets hope I work!")
